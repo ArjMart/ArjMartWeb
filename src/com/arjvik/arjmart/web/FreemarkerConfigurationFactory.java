0af8749a2a4cac9be33747657200ff8f5f8b1877
Arjun Vikram <arjvik@gmail.com>
2018-06-05 12:30:19 -0500

    Fixed FreemarkerConfigurationFactory

--- src/com/arjvik/arjmart/web/FreemarkerConfigurationFactory.java
+++ src/com/arjvik/arjmart/web/FreemarkerConfigurationFactory.java
@@ -1,5 +1,6 @@
 package com.arjvik.arjmart.web;
 
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerDefaultConfigurationFactory;
@@ -8,6 +9,7 @@ import freemarker.template.TemplateExceptionHandler;
 
 public class FreemarkerConfigurationFactory extends FreemarkerDefaultConfigurationFactory implements org.glassfish.jersey.server.mvc.freemarker.FreemarkerConfigurationFactory {
 
+	@Inject
 	public FreemarkerConfigurationFactory(ServletContext servletContext) {
 		super(servletContext);
 		configuration.setTemplateExceptionHandler(TemplateExceptionHandler.DEBUG_HANDLER);
