899fabeee514c67960dc78270a719f87bf868b7e
Arjun Vikram <arjvik@gmail.com>
2018-07-03 14:40:44 -0500

    Updated dependencies to Jersey version 2.27

--- src/com/arjvik/arjmart/web/ResourceConfig.java
+++ src/com/arjvik/arjmart/web/ResourceConfig.java
@@ -2,12 +2,9 @@ package com.arjvik.arjmart.web;
 
 import org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature;
 
-//@ApplicationPath("/")
 public class ResourceConfig extends org.glassfish.jersey.server.ResourceConfig {
 	public ResourceConfig(){
 		packages("com.arjvik.arjmart.web");
 		register(FreemarkerMvcFeature.class);
-		//register(Hk2Binder.class);
-		//register(Hk2Feature.class);
 	}
 }
